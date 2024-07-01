647110addc9db54f15b13c4b678a96e72b7affc2
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2024-07-01 17:08:40 +0100

    Adding InpuModels on API layer.

---
+++ DevFreela.API/InputModels/CreateUserInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.API.InputModels
+{
+    public class CreateUserInputModel
+    {
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime BithDate { get; set; }
+    }
+}
